8d5efe78b1b90945b9bc3ba176b899ceddb425b4
Destinyddd <22300180004@m.fudan.edu.cn>
2024-06-22 18:00:02 +0800

    update

--- Sql-Online-Judge-Front/src/components/student/Contest.vue
+++ Sql-Online-Judge-Front/src/components/student/Contest.vue
@@ -1,27 +1,65 @@
 <template>
-
-  <div>
-    <div class="nav">
-    <Navbar />
-  </div>
-    <h1>Contest Page</h1>
-    <p>This is the Contest page. Test your navigation here.</p>
+  <div class="contest-list">
+    <h1>Available Contests</h1>
+    <ul>
+      <li v-for="contest in contests" :key="contest.id">
+        <h2>{{ contest.name }} ({{ contest.category }}) - [ID: {{ contest.id }}]</h2>
+        <button @click="enterContest(contest)" :disabled="!contest.hasAccess">
+          Enter
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/student/Navbar.vue';
-
 export default {
-  name: 'Contest',
-  components: {
-    Navbar
+  data() {
+    return {
+      contests: []
+    };
+  },
+  created() {
+    this.fetchContests();
+  },
+  methods: {
+    fetchContests() {
+      // 假设有一个 API 来获取比赛列表和访问权限
+      // 这里的数据应该从后端获取，以下是示例数据
+      this.contests = [
+        { id: 1, name: 'Mathematics Olympiad', category: 'Mathematics', hasAccess: true },
+        { id: 2, name: 'Physics Contest', category: 'Physics', hasAccess: false },
+        { id: 3, name: 'Chemistry Challenge', category: 'Chemical', hasAccess: true }
+      ];
+    },
+    enterContest(contest) {
+      if (contest.hasAccess) {
+        // 例如，可以用路由导航到比赛详情页
+        this.$router.push(`/contests/${contest.id}`);
+      } else {
+        alert("You do not have access to enter this contest.");
+      }
+    }
   }
-};
+}
 </script>
 
 <style scoped>
-h1 {
-  color: #2c3e50;
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+}
+
+button {
+  margin-top: 5px;
+}
+
+button:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
 }
 </style>
