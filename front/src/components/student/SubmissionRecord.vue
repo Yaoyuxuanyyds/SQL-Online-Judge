a5fd463065f36c8b976011909993e90a0852db70
WS-3917 <ws3917@outlook.com>
2024-06-23 20:29:55 +0800

    .

--- front/src/components/student/SubmissionRecord.vue
+++ front/src/components/student/SubmissionRecord.vue
@@ -1,9 +1,9 @@
 <template>
   <tr class="submission-record">
-    <td>{{ record.submissionTime | formatDate }}</td>
-    <td>{{ record.problemId }}</td>
-    <td>{{ record.submissionContent }}</td>
-    <td>{{ record.result }}</td>
+    <td>{{ record.submit_time | formatDate }}</td>
+    <td>{{ record.student_id }}</td>
+    <td>{{ record.submit_sql }}</td>
+    <td>{{ record.status }}</td>
   </tr>
 </template>
 
